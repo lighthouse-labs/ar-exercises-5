f60cdea7f5caba32962aa8b6fc0c079cef755541
Amina Malik <amina.malik@telus.com>
2023-01-23 23:26:49 -0500

    first commit ex 01-07

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Your coLoad the first store (with id = 1) from the database and assign it to an instance variable @store1.
+=begin
+Load the second store from the database and assign it to @store2.
+Update the first store (@store1) instance in the database. (Change its name or something.)de goes here ...
+=end
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.update(name: "Narnia")
+
+pp Store.find_by(id: 1)
